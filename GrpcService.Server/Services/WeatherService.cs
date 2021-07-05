7a05dbf8924e241debf92b04731adc1af191b27c
Pablo <p.c.ramirez5150@gmail.com>
2021-07-05 21:47:53 +0200

    Adds gRPC brief description to README.md

--- GrpcService.Server/Services/WeatherService.cs
+++ GrpcService.Server/Services/WeatherService.cs
@@ -1,5 +1,7 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using Grpc.Core;
 using GrpcService.Server.Contracts;
@@ -17,10 +19,14 @@ namespace GrpcService.Server.Services
         
         public override async Task<WeatherResponse> GetCurrentWeather(GetCurrentWeatherForCity request, ServerCallContext context)
         {
+            Console.WriteLine($"Current Thread: {Thread.CurrentThread.ManagedThreadId}");
+            
             var httpClient = _httpClientFactory.CreateClient();
             var uri = $"https://api.openweathermap.org/data/2.5/weather?q={request.City}&appid=33437061427810ad3d26f6ae0ebac594&units={request.Units}";
-            var responseText = await httpClient.GetStringAsync(uri);
+            var responseText = await httpClient.GetStringAsync(uri).ConfigureAwait(false);
 
+            Console.WriteLine($"Continuation Thread: {Thread.CurrentThread.ManagedThreadId}");
+            
             var temperatures = JsonSerializer.Deserialize<Temperatures>(responseText);
             return new WeatherResponse
             {
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # GrpcGettingStarted
 Grpc exploration via getting started tutorial
+
+Z>gRPC is a open source high performance Remote Procedure Call (RPC) that is agnostic from any programming language.
+It can efficiently connect services in and across data centers with pluggable support for load balancing, tracing, health checking and authentication.
+
+It provides a nice set of tools to make it very performant, 
+for example selective message compression. For example, if you are streaming mixed text and images over a single stream (or really any mixed compressible content), 
+you can turn off compression for the images. This saves you from compressing already compressed data which won't get any smaller, but will burn up your CPU.
+
+However when trying to achieve loosely coupled services there could be a better option using more Event-Driven Architecture and asynchronous alternatives, using a message bus to 
